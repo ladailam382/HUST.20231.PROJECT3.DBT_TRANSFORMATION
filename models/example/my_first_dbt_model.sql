c7316d63aa846cdcec2dc9131b6f23c4913a4bb8
ladailam382 <ladailam382@gmail.com>
2023-10-20 10:59:23 +0700

    modify model

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -11,9 +11,26 @@
 
 with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
+    select
+    _airbyte_unique_key,
+    _ab_cdc_log_file,
+    iata,
+    base_airport,
+    _ab_cdc_log_pos,
+    _ab_cdc_updated_at,
+    _ab_cdc_deleted_at,
+    airline_id,
+    airlinename,
+    _ab_cdc_cursor,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    getdate() as _airbyte_normalized_at,
+    _airbyte_airline_hashid
+    from "dev".public."airline_scd"
+    -- airline from "dev".public._airbyte_raw_airline
+    where 1 = 1
+    and _airbyte_active_row = 1
+
 
 )
 
